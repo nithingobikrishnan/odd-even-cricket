08eb59434d2b7f64f1ea7d1ef10d6f9776aa5909
nithingobikrishnan <nithin.gobikrishnan@gmail.com>
2026-04-24 12:48:47 +0530

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,27 +1,17 @@
-export function botMove(state){
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getDatabase, ref, set, onValue, update } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
- let max = state.maxNumber;
+const firebaseConfig = {
+  apiKey: "AIzaSyCrVyYIQk7pgMW05n4gYgTFwcLGZEh3IdI",
+  authDomain: "odd-even-cricket-6fcb7.firebaseapp.com",
+  databaseURL: "https://odd-even-cricket-6fcb7-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "odd-even-cricket-6fcb7",
+  storageBucket: "odd-even-cricket-6fcb7.appspot.com",
+  messagingSenderId: "56937015712",
+  appId: "1:56937015712:web:93d3d5ed49ac99b13a5cc5"
+};
 
- // pressure logic
- if(state.innings===2){
-  let ballsLeft = (state.oversLimit*6) - state.ball;
-  let runsLeft = state.target - state.score;
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
-  if(ballsLeft <= 6){
-    return Math.floor(Math.random()*max);
-  }
-
-  if(runsLeft > ballsLeft){
-    return Math.floor(Math.random()*max); // aggressive
-  }
- }
-
- // pattern read
- let last = state.history[state.history.length-1] || 0;
-
- if(Math.random() < 0.4){
-  return last; // try wicket
- }
-
- return Math.floor(Math.random()*(max+1));
-}
+export { db, ref, set, onValue, update };
