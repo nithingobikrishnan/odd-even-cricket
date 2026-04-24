5d689fdc36865887fb03ea2e99a4fc1a2a399a41
nithingobikrishnan <nithin.gobikrishnan@gmail.com>
2026-04-24 12:15:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getDatabase, ref, set, onValue, update } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+
 const firebaseConfig = {
- apiKey:"AIzaSyCrVyYIQk7pgMW05n4gYgTFwcLGZEh3IdI",
- databaseURL:"https://odd-even-cricket-6fcb7-default-rtdb.asia-southeast1.firebasedatabase.app"
+  apiKey: "AIzaSyCrVyYIQk7pgMW05n4gYgTFwcLGZEh3IdI",
+  authDomain: "odd-even-cricket-6fcb7.firebaseapp.com",
+  projectId: "odd-even-cricket-6fcb7",
+  storageBucket: "odd-even-cricket-6fcb7.firebasestorage.app",
+  messagingSenderId: "56937015712",
+  appId: "1:56937015712:web:93d3d5ed49ac99b13a5cc5"
 };
 
-if(!firebase.apps.length){
- firebase.initializeApp(firebaseConfig);
-}
-const db = firebase.database();
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
-function getRoom(){
- return new URLSearchParams(location.search).get("room");
+export { db, ref, set, onValue, update };
+
+export function getUsername(){
+ let u = localStorage.getItem("username");
+ if(!u){
+  u = prompt("Enter name") || "Player";
+  localStorage.setItem("username", u);
+ }
+ return u;
 }
