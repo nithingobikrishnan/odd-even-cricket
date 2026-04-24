4e83b0a63929b99f37cccd0fc3a14fc41d6967b6
nithingobikrishnan <nithin.gobikrishnan@gmail.com>
2026-04-24 15:54:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,8 @@ const firebaseConfig = {
   authDomain: "odd-even-cricket-6fcb7.firebaseapp.com",
   databaseURL: "https://odd-even-cricket-6fcb7-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "odd-even-cricket-6fcb7",
-  storageBucket: "odd-even-cricket-6fcb7.appspot.com",
-  messagingSenderId: "56937015712",
-  appId: "1:56937015712:web:93d3d5ed49ac99b13a5cc5"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-export { db, ref, set, onValue, update };
+export const db = getDatabase(app);
+export { ref, set, onValue, update };
