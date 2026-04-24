3e6125deb32255400740daa0c6e6d2e78f09c137
nithingobikrishnan <nithin.gobikrishnan@gmail.com>
2026-04-24 12:49:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ import { getDatabase, ref, set, onValue, update } from "https://www.gstatic.com/
 const firebaseConfig = {
   apiKey: "AIzaSyCrVyYIQk7pgMW05n4gYgTFwcLGZEh3IdI",
   authDomain: "odd-even-cricket-6fcb7.firebaseapp.com",
+  databaseURL: "https://odd-even-cricket-6fcb7-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "odd-even-cricket-6fcb7",
-  storageBucket: "odd-even-cricket-6fcb7.firebasestorage.app",
+  storageBucket: "odd-even-cricket-6fcb7.appspot.com",
   messagingSenderId: "56937015712",
   appId: "1:56937015712:web:93d3d5ed49ac99b13a5cc5"
 };
